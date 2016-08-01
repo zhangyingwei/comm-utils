4f23a584d8b31ef1956345571744b389ede667d3
zhangyingwei <723129bao>
2016-08-01 17:28:48 +0800

    comm-utils

---
+++ src/test/java/com/zhangyw/utils/StringUtilTest.java
@@ -0,0 +1,16 @@
+package com.zhangyw.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+public class StringUtilTest {
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		String s1 = "啊";
+		String s2 = "我";
+		String s3 = "我在杭州,你在哪里";
+		System.out.println(StringUtil.getAllBeginCharacter(s3));
+		System.out.println(new String("\u554A"));
+		System.out.println(Arrays.toString(s1.getBytes()));
+		System.out.println(Arrays.toString(s2.getBytes()));
+	}
+}
