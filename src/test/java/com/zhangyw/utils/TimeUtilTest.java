112f08ea39d2f5012de3cf130187c08577a73ca2
zhangyingwei <723129bao>
2016-08-04 18:08:26 +0800

    timeutil

--- src/test/java/com/zhangyw/utils/TimeUtilTest.java
+++ src/test/java/com/zhangyw/utils/TimeUtilTest.java
@@ -1,5 +1,7 @@
 package com.zhangyw.utils;
 
+import java.util.Date;
+
 import com.zhangyw.utils.exception.TimeStringFormateException;
 import com.zhangyw.utils.time.TimeUtil;
 
@@ -7,5 +9,6 @@ public class TimeUtilTest {
 	public static void main(String[] args) throws TimeStringFormateException {
 		String time = "2015-08-09T09:23:29.000Z";
 		System.out.println(TimeUtil.fromString(time).getTime());
+		System.out.println(new Date().toString());
 	}
 }
